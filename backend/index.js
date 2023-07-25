4a15c0f680f16644e44c6c31a8fc9c26a6c1a1b1
Nikhil Singh <msit.nikhil@gmail.com>
2023-07-25 14:26:48 +0100

    creating shapeGrid

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const [shapeGrid]= require('./shapeGrid')
 const app = express()
 const port = 3000
 
---
+++ backend/shapeGrid.js
@@ -0,0 +1,34 @@
+
+const shapeGrid  = [
+    {shapeId: '101', shapeType: 'circle', shapeSize: 'small', producedFruit: false},
+    {shapeId: '102', shapeType: 'circle', shapeSize: 'medium', producedFruit: false},
+    {shapeId: '103', shapeType: 'circle', shapeSize: 'large', producedFruit: false},
+    {shapeId: '104', shapeType: 'circle', shapeSize: 'small', producedFruit: false},
+    {shapeId: '105', shapeType: 'circle', shapeSize: 'medium', producedFruit: false},
+    {shapeId: '106', shapeType: 'circle', shapeSize: 'large', producedFruit: false},
+    {shapeId: '107', shapeType: 'circle', shapeSize: 'small', producedFruit: false},
+    {shapeId: '108', shapeType: 'circle', shapeSize: 'medium', producedFruit: false},
+    {shapeId: '109', shapeType: 'circle', shapeSize: 'large', producedFruit: false},
+    {shapeId: '110', shapeType: 'circle', shapeSize: 'small', producedFruit: false},
+    {shapeId: '111', shapeType: 'circle', shapeSize: 'medium', producedFruit: false},
+    {shapeId: '112', shapeType: 'circle', shapeSize: 'large', producedFruit: false},
+    {shapeId: '113', shapeType: 'circle', shapeSize: 'small', producedFruit: false},
+    {shapeId: '114', shapeType: 'circle', shapeSize: 'medium', producedFruit: false},
+    {shapeId: '115', shapeType: 'circle', shapeSize: 'large', producedFruit: false},
+    {shapeId: '116', shapeType: 'circle', shapeSize: 'small', producedFruit: false},
+    {shapeId: '117', shapeType: 'circle', shapeSize: 'medium', producedFruit: false},
+    {shapeId: '118', shapeType: 'circle', shapeSize: 'large', producedFruit: false},
+    {shapeId: '119', shapeType: 'null', shapeSize: 'null', producedFruit: 'null'},
+    {shapeId: '120', shapeType: 'null', shapeSize: 'null', producedFruit: 'null'},
+    {shapeId: '121', shapeType: 'null', shapeSize: 'null', producedFruit: 'null'},
+    {shapeId: '122', shapeType: 'null', shapeSize: 'null', producedFruit: 'null'},
+    {shapeId: '123', shapeType: 'null', shapeSize: 'null', producedFruit: 'null'},
+    {shapeId: '124', shapeType: 'null', shapeSize: 'null', producedFruit: 'null'},
+    {shapeId: '125', shapeType: 'null', shapeSize: 'null', producedFruit: 'null'}
+];
+
+
+
+module.exports = {
+shapeGrid
+};
